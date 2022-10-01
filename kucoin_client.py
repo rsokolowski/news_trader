7b4674bf4f5504b70464eac35c01078efc7ed3ee
rsokolowski <rafal.sokolowski87@gmail.com>
2022-10-01 22:24:38 +0200

    commit

--- kucoin_client.py
+++ kucoin_client.py
@@ -2,12 +2,12 @@ from kucoin.client import Client
 from keys import keys
 
 API_NAME = 'news_trading'
-SPOT_API_KEY = keys.get('SPOT_API_KEY') #'6335828f25e651000131fd89'
-SPOT_API_SECRET = keys.get('SPOT_API_SECRET') # '5831f0a1-e8ce-4d98-bf03-c894aa19fa3a'
-SPOT_API_PASSPHRASE = keys.get('SPOT_API_PASSPHRASE') #'EERGmqdrpfDA7ZV'
+SPOT_API_KEY = keys.get('SPOT_API_KEY') 
+SPOT_API_SECRET = keys.get('SPOT_API_SECRET') 
+SPOT_API_PASSPHRASE = keys.get('SPOT_API_PASSPHRASE') 
 
-FUTURES_API_KEY = keys.get('FUTURES_API_KEY') # '633585d48209b100011843e8'
-FUTURES_API_SECRET = keys.get('FUTURES_API_SECRET') # 'a261919f-b327-4374-8db2-caa3c3f875f3'
+FUTURES_API_KEY = keys.get('FUTURES_API_KEY') 
+FUTURES_API_SECRET = keys.get('FUTURES_API_SECRET') 
 
 class KuCoinClient(object):
     
